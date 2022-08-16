f63061d24707cccefaaf0fb462863e80dc57bcea
JainerPinta <jainer.pinta@unl.edu.ec>
2022-08-15 21:05:23 -0500

    Añadir evento

--- modelo/models.py
+++ modelo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    listaTiposE=(
+        ('Conferencia'),('Convención'),('Celebración'),('Boda'),('Banquete')
+    )
+    evento_id = models.AutoField(primary_key=True)
+    tipoEvento = models.CharField(max_length=20, choices=listaTiposE)
+    fechaInicio = models.DateTimeField(auto_now_add=True, null = False)
+    fechaFin = models.DateTimeField(auto_now_add=True, null = False)
+    numInvitados = models.IntegerField(max_length=100, null = False)
