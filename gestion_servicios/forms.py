307160c05247f211a4638d0508ba10957120a476
JainerPinta <jainer.pinta@unl.edu.ec>
2022-08-16 03:02:00 -0500

    Corregir errores

--- gestion_servicios/forms.py
+++ gestion_servicios/forms.py
@@ -1,8 +1,13 @@
-from dataclasses import fields
 from django import forms
 from modelo.models import Servicio
 
 class ServicioFormulario(forms.ModelForm):
     class Meta:
         model = Servicio
-        fields = ['tipo','cliente','precio','uso']
+        fields = ["tipo","cliente","precio","uso"]
+        widgets = {
+            'tipo': forms.TextInput(attrs={'placeholder':'Ingrese tipo', 'class': 'form-control'}),
+            'cliente': forms.TextInput(attrs={'placeholder':'Ingrese cedula', 'class': 'form-control'}),
+            'precio': forms.NumberInput(attrs={'placeholder':'Ingrese precio', 'class': 'form-control'}),
+            'uso': forms.NumberInput(attrs={'placeholder':'Ingrese uso en horas', 'class': 'form-control'}),
+        }
--- gestion_servicios/views.py
+++ gestion_servicios/views.py
@@ -1,21 +1,18 @@
-from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from modelo.models import Servicio, Cliente
 from .forms import ServicioFormulario
-from django.shortcuts import render
-from django.contrib.auth.models import User, Group
 
 # Create your views here.
 def registrar(request):
     formulario_servicio = ServicioFormulario(request.POST)
-    listaCliente = Cliente.objects.all()
-    if request.method == 'POST':
-        if formulario_servicio.is_valid():
-            servicio = Servicio()
-            datos_servicio = formulario_servicio.cleaned_data
-            servicio.tipo = datos_servicio.get('tipo')
-            servicio.cliente = datos_servicio.get('cliente')
-            servicio.precio = datos_servicio.get('precio')
-            servicio.uso = datos_servicio.get('uso')
-            servicio.save()
+    if request.method == 'POST' and formulario_servicio.is_valid():
+        servicio = Servicio()
+        datos_servicio = formulario_servicio.cleaned_data
+        servicio.tipo = datos_servicio.get('tipo')
+        servicio.cliente = datos_servicio.get('cliente')
+        servicio.precio = datos_servicio.get('precio')
+        servicio.uso = datos_servicio.get('uso')
+        servicio.save()
+    else:
+        print("No validos")
     return render(request, 'servicios/guiRegistrarServicio.html', locals())
