0b544f2d11b061f72aa8c6e46fb237a3d18f86b5
JainerPinta <jainer.pinta@unl.edu.ec>
2022-08-15 22:17:15 -0500

    Actualizar clases

--- modelo/models.py
+++ modelo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
-# Create your models here.
-class Evento(models.Model):
-    listaTiposE=(
-        ('Conferencia'),('Convención'),('Celebración'),('Boda'),('Banquete')
-    )
-    evento_id = models.AutoField(primary_key=True)
-    tipoEvento = models.CharField(max_length=20, choices=listaTiposE, null = False, default='Celebración')
-    fechaInicio = models.DateTimeField(auto_now_add=True, null = False)
-    fechaFin = models.DateTimeField(auto_now_add=True, null = False)
-    numInvitados = models.IntegerField(max_length=100, null = False)
+class Cliente(models.Model):
+    cliente_id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length=30,null = False)
+    cedula = models.CharField(max_length=10,null = False)
+    
+
+class Servicio(models.Model):
+    servicio_id = models.AutoField(primary_key = True)
+    tipo = models.CharField(max_length=25,null = False)
+    cliente = models.CharField(max_length=25,null = False)
+    precio = models.FloatField(max_length=10,null = False)
+    uso = models.IntegerField()
