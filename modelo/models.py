0be9747fda11a9a2abffae0ac24b2da09ae963b9
JainerPinta <jainer.pinta@unl.edu.ec>
2022-08-15 21:45:03 -0500

    Revertir cambios

--- modelo/models.py
+++ modelo/models.py
@@ -6,7 +6,7 @@ class Evento(models.Model):
         ('Conferencia'),('Convención'),('Celebración'),('Boda'),('Banquete')
     )
     evento_id = models.AutoField(primary_key=True)
-    tipoEvento = models.CharField(max_length=20, choices=listaTiposE)
+    tipoEvento = models.CharField(max_length=20, choices=listaTiposE, null = False, default='Celebración')
     fechaInicio = models.DateTimeField(auto_now_add=True, null = False)
     fechaFin = models.DateTimeField(auto_now_add=True, null = False)
     numInvitados = models.IntegerField(max_length=100, null = False)
--- swHotelero/settings.py
+++ swHotelero/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'swHotelero.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'hotelCardenal',
+        'USER':'usuarioEvento',
+        'PASSWORD':'eventos45-',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
 
